47958284e5d2f3570e1f331356865c28f61e9c21
James Mills <prologic@shortcircuit.net.au>
2016-07-26 00:24:36 -0700

    [tests]: Fix tests/node/test_server.py::test_auto_reconnect

--- tests/node/test_server.py
+++ tests/node/test_server.py
@@ -61,12 +61,11 @@ def test_auto_reconnect(app, watcher, manager):
     node = Node().register(client)
     chan = node.add('client1', *app.bind, reconnect_delay=1, connect_timeout=1)
     assert watcher.wait('connected', channel=chan)
+    watcher.clear()
 
     # close server
     app.fire(close(), app.channel)
     assert watcher.wait('closed', channel=app.channel)
-    watcher.clear()
-
     assert watcher.wait('connected', channel=chan)
 
     client.unregister()
